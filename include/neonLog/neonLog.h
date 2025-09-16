a355d0a8d24cba3cec8d4b167f7840ab20212082
Alik-Green <alikgreen@proton.me>
2025-09-16 22:08:49 +0100

    made include file work

--- include/neonLog/neonLog.h
+++ include/neonLog/neonLog.h
@@ -3,3 +3,10 @@
 #include "debug.h"
 #include "level.h"
 #include "log.h"
+
+#include "timers/timer.h"
+#include "timers/scopeTimer.h"
+
+#include "streams/stream.h"
+#include "streams/consoleStream.h"
+#include "streams/fileStream.h"
